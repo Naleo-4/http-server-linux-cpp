bfb2458af8785713c6ac0268f5ce6538035f14a2
Naleo-4 <anhbndc2004@gmail.com>
2024-06-24 11:20:13 -0400

    test

--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,4 @@
+#include "server.h"
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -8,69 +9,125 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+std::string get_line(const std::string& str, int& cur)
+{
+    std::string temp_str;
+    while (str[cur] != '\r')
+    {
+        temp_str += str[cur++];
+    }
+    cur+=2;
+    return temp_str;
+}
 
+// std::string check_request(const std::string& str, ssize_t length)
+// {
+//     using namespace std;
+//     int cur{0},sub_cur{0};
+//     string data{},sub{},sub2{},temp{str};
+//     while (cur < length && !temp.empty())
+//     {
+//         cur = temp.find_first_of("\r\n");
+//         sub = temp.substr(0,cur);
+//         data.append(sub);
+//         // data += ' ';
+//         temp = temp.substr(cur+2);
+//         // while (true)
+//         // {
+//         //     sub_cur = sub.find_first_of(' ');
+//         //     data = sub.substr(0,sub_cur);
+//         //     cout << data << "\n";
+//         //     {
+//         //
+//         //     }
+//         //     if (sub_cur == sub.npos) break;
+//         //     sub = sub.substr(sub_cur+1);
+//         // }
+//     }
+//     // data.pop_back();
+//     // return data;
+// }
+STATUS_CODE check_request_target(const std::string& str)
+{
+    int cur = str.find_first_of('/');
+    if (str[cur+1] != ' ')
+        return  NOT_FOUND;
+    return OK;
+}
 
-int main(int argc, char **argv) {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  // std::cout << "Logs from your program will appear here!\n";
+int main(int argc, char** argv)
+{
+    // Flush after every std::cout / std::cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // std::cout << "Logs from your program will appear here!\n";
 
-  const int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
-  }
+    // Uncomment this block to pass the first stage
 
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
-  constexpr int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
+    const int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0)
+    {
+        std::cerr << "Failed to create server socket\n";
+        return 1;
+    }
 
-  sockaddr_in server_addr{};
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(4221);
-  if (bind(server_fd, (sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 4221\n";
-    return 1;
-  }
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // ensures that we don't run into 'Address already in use' errors
+    constexpr int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+    {
+        std::cerr << "setsockopt failed\n";
+        return 1;
+    }
 
-  constexpr int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
+    sockaddr_in server_addr{};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(4221);
+    if (bind(server_fd, (sockaddr*)&server_addr, sizeof(server_addr)) != 0)
+    {
+        std::cerr << "Failed to bind to port 4221\n";
+        return 1;
+    }
 
-  sockaddr_in client_addr{};
-  int client_addr_len = sizeof(client_addr);
+    constexpr int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0)
+    {
+        std::cerr << "listen failed\n";
+        return 1;
+    }
 
-  std::cout << "Waiting for a client to connect...\n";
+    sockaddr_in client_addr{};
+    int client_addr_len = sizeof(client_addr);
 
-  int client_fd = accept(server_fd, (sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    std::cout << "Waiting for a client to connect...\n";
+    while (true)
+    {
+        int client_fd = accept(server_fd, (sockaddr*)&client_addr, (socklen_t*)&client_addr_len);
 
-  constexpr ssize_t BUFF_LENGTH = 2048;
-  char buff[];
-  ssize_t rBuff = recv(client_fd, buff, BUFF_LENGTH,0);
-  if (rBuff < 0)
-  {
-    std::cerr << "error recceving message from client\n";
-    close(client_fd);
-  }
-  std::string str(buff);
-  std::cout << buff;
-  std::string message = "HTTP/1.1 200 OK\r\n\r\n";
-  send(client_fd,message.c_str(), message.length(),0);
-  std::cout << "Client connected\n";
+        constexpr ssize_t BUFF_LENGTH = 2048;
+        char buff[BUFF_LENGTH]{};
+        ssize_t rBuff = recv(client_fd, buff, BUFF_LENGTH, 0);
+        if (rBuff < 0)
+        {
+            std::cerr << "error recceving message from client\n";
+            close(client_fd);
+        }
+        std::string str(buff, rBuff);
 
-  close(server_fd);
+        // std::string data{check_request(str,rBuff)};
+        // std::cout << data;
+        STATUS_CODE status{check_request_target(str)};
+        std::string message = "HTTP/1.1 " + std::to_string(status) + ' ' + status_code_to_string(status) + "\r\n\r\n";
+        send(client_fd, message.c_str(), message.length(), 0);
+        std::cout << "Client connected\n";
+        close(client_fd);
+        std::cout << "Client closed\n";
+    }
 
-  return 0;
+    close(server_fd);
+
+    return 0;
 }
--- src/server.h
+++ src/server.h
@@ -5,4 +5,150 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <string>
+enum STATUS_CODE
+{
+    // Informational responses
+    CONTINUE                        = 100,
+    SWITCHING_PROTOCOLS             = 101,
+    PROCESSING                      = 102,
+    EARLY_HINTS                     = 103,
+
+    // Successful responses
+    OK                              = 200,
+    CREATED                         = 201,
+    ACCEPTED                        = 202,
+    NON_AUTHORITATIVE_INFORMATION   = 203,
+    NO_CONTENT                      = 204,
+    RESET_CONTENT                   = 205,
+    PARTIAL_CONTENT                 = 206,
+    MULTI_STATUS                    = 207,
+    ALREADY_REPORTED                = 208,
+    IM_USED                         = 226,
+
+    // Redirection messages
+    MULTIPLE_CHOICES                = 300,
+    MOVED_PERMANENTLY               = 301,
+    FOUND                           = 302,
+    SEE_OTHER                       = 303,
+    NOT_MODIFIED                    = 304,
+    USE_PROXY                       = 305,  // Deprecated
+    UNUSED                          = 306,  // No longer used
+    TEMPORARY_REDIRECT              = 307,
+    PERMANENT_REDIRECT              = 308,
+
+    // Client error responses
+    BAD_REQUEST                     = 400,
+    UNAUTHORIZED                    = 401,
+    PAYMENT_REQUIRED                = 402,  // Experimental
+    FORBIDDEN                       = 403,
+    NOT_FOUND                       = 404,
+    METHOD_NOT_ALLOWED              = 405,
+    NOT_ACCEPTABLE                  = 406,
+    PROXY_AUTHENTICATION_REQUIRED   = 407,
+    REQUEST_TIMEOUT                 = 408,
+    CONFLICT                        = 409,
+    GONE                            = 410,
+    LENGTH_REQUIRED                 = 411,
+    PRECONDITION_FAILED             = 412,
+    PAYLOAD_TOO_LARGE               = 413,
+    URI_TOO_LONG                    = 414,
+    UNSUPPORTED_MEDIA_TYPE          = 415,
+    RANGE_NOT_SATISFIABLE           = 416,
+    EXPECTATION_FAILED              = 417,
+    IM_A_TEAPOT                     = 418,
+    MISDIRECTED_REQUEST             = 421,
+    UNPROCESSABLE_CONTENT           = 422,
+    LOCKED                          = 423,
+    FAILED_DEPENDENCY               = 424,
+    TOO_EARLY                       = 425,  // Experimental
+    UPGRADE_REQUIRED                = 426,
+    PRECONDITION_REQUIRED           = 428,
+    TOO_MANY_REQUESTS               = 429,
+    REQUEST_HEADER_FIELDS_TOO_LARGE = 431,
+    UNAVAILABLE_FOR_LEGAL_REASONS   = 451,
+
+    // Server error responses
+    INTERNAL_SERVER_ERROR           = 500,
+    NOT_IMPLEMENTED                 = 501,
+    BAD_GATEWAY                     = 502,
+    SERVICE_UNAVAILABLE             = 503,
+    GATEWAY_TIMEOUT                 = 504,
+    HTTP_VERSION_NOT_SUPPORTED      = 505,
+    VARIANT_ALSO_NEGOTIATES         = 506,
+    INSUFFICIENT_STORAGE            = 507,
+    LOOP_DETECTED                   = 508,
+    NOT_EXTENDED                    = 510,
+    NETWORK_AUTHENTICATION_REQUIRED = 511
+};
+
+constexpr std::string status_code_to_string(const STATUS_CODE code) {
+    switch(code) {
+        case CONTINUE:                        return "CONTINUE";
+        case SWITCHING_PROTOCOLS:             return "SWITCHING_PROTOCOLS";
+        case PROCESSING:                      return "PROCESSING";
+        case EARLY_HINTS:                     return "EARLY_HINTS";
+        case OK:                              return "OK";
+        case CREATED:                         return "CREATED";
+        case ACCEPTED:                        return "ACCEPTED";
+        case NON_AUTHORITATIVE_INFORMATION:   return "NON_AUTHORITATIVE_INFORMATION";
+        case NO_CONTENT:                      return "NO_CONTENT";
+        case RESET_CONTENT:                   return "RESET_CONTENT";
+        case PARTIAL_CONTENT:                 return "PARTIAL_CONTENT";
+        case MULTI_STATUS:                    return "MULTI_STATUS";
+        case ALREADY_REPORTED:                return "ALREADY_REPORTED";
+        case IM_USED:                         return "IM_USED";
+        case MULTIPLE_CHOICES:                return "MULTIPLE_CHOICES";
+        case MOVED_PERMANENTLY:               return "MOVED_PERMANENTLY";
+        case FOUND:                           return "FOUND";
+        case SEE_OTHER:                       return "SEE_OTHER";
+        case NOT_MODIFIED:                    return "NOT_MODIFIED";
+        case USE_PROXY:                       return "USE_PROXY";  // Deprecated
+        case UNUSED:                          return "UNUSED";  // No longer used
+        case TEMPORARY_REDIRECT:              return "TEMPORARY_REDIRECT";
+        case PERMANENT_REDIRECT:              return "PERMANENT_REDIRECT";
+        case BAD_REQUEST:                     return "BAD_REQUEST";
+        case UNAUTHORIZED:                    return "UNAUTHORIZED";
+        case PAYMENT_REQUIRED:                return "PAYMENT_REQUIRED";  // Experimental
+        case FORBIDDEN:                       return "FORBIDDEN";
+        case NOT_FOUND:                       return "NOT_FOUND";
+        case METHOD_NOT_ALLOWED:              return "METHOD_NOT_ALLOWED";
+        case NOT_ACCEPTABLE:                  return "NOT_ACCEPTABLE";
+        case PROXY_AUTHENTICATION_REQUIRED:   return "PROXY_AUTHENTICATION_REQUIRED";
+        case REQUEST_TIMEOUT:                 return "REQUEST_TIMEOUT";
+        case CONFLICT:                        return "CONFLICT";
+        case GONE:                            return "GONE";
+        case LENGTH_REQUIRED:                 return "LENGTH_REQUIRED";
+        case PRECONDITION_FAILED:             return "PRECONDITION_FAILED";
+        case PAYLOAD_TOO_LARGE:               return "PAYLOAD_TOO_LARGE";
+        case URI_TOO_LONG:                    return "URI_TOO_LONG";
+        case UNSUPPORTED_MEDIA_TYPE:          return "UNSUPPORTED_MEDIA_TYPE";
+        case RANGE_NOT_SATISFIABLE:           return "RANGE_NOT_SATISFIABLE";
+        case EXPECTATION_FAILED:              return "EXPECTATION_FAILED";
+        case IM_A_TEAPOT:                     return "IM_A_TEAPOT";
+        case MISDIRECTED_REQUEST:             return "MISDIRECTED_REQUEST";
+        case UNPROCESSABLE_CONTENT:           return "UNPROCESSABLE_CONTENT";
+        case LOCKED:                          return "LOCKED";
+        case FAILED_DEPENDENCY:               return "FAILED_DEPENDENCY";
+        case TOO_EARLY:                       return "TOO_EARLY";  // Experimental
+        case UPGRADE_REQUIRED:                return "UPGRADE_REQUIRED";
+        case PRECONDITION_REQUIRED:           return "PRECONDITION_REQUIRED";
+        case TOO_MANY_REQUESTS:               return "TOO_MANY_REQUESTS";
+        case REQUEST_HEADER_FIELDS_TOO_LARGE: return "REQUEST_HEADER_FIELDS_TOO_LARGE";
+        case UNAVAILABLE_FOR_LEGAL_REASONS:   return "UNAVAILABLE_FOR_LEGAL_REASONS";
+        case INTERNAL_SERVER_ERROR:           return "INTERNAL_SERVER_ERROR";
+        case NOT_IMPLEMENTED:                 return "NOT_IMPLEMENTED";
+        case BAD_GATEWAY:                     return "BAD_GATEWAY";
+        case SERVICE_UNAVAILABLE:             return "SERVICE_UNAVAILABLE";
+        case GATEWAY_TIMEOUT:                 return "GATEWAY_TIMEOUT";
+        case HTTP_VERSION_NOT_SUPPORTED:      return "HTTP_VERSION_NOT_SUPPORTED";
+        case VARIANT_ALSO_NEGOTIATES:         return "VARIANT_ALSO_NEGOTIATES";
+        case INSUFFICIENT_STORAGE:            return "INSUFFICIENT_STORAGE";
+        case LOOP_DETECTED:                   return "LOOP_DETECTED";
+        case NOT_EXTENDED:                    return "NOT_EXTENDED";
+        case NETWORK_AUTHENTICATION_REQUIRED: return "NETWORK_AUTHENTICATION_REQUIRED";
+        default:                              return "UNKNOWN_STATUS_CODE";
+    }
+}
+
 #endif //SERVER_H
